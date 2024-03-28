db0728c10f0484a6a099ff9c22bded1ab1d6872a
superhan-dev <superhan.dev@gmail.com>
2024-03-28 23:53:37 +1100

    update hero section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
-gsap.config({ trialWarn: false });
+// gsap.config({ trialWarn: false });
 // gsap.registerPlugin(ScrollTrigger, SplitText);
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 // const split = new SplitText("p", { type: "lines" });
 
@@ -17,32 +17,32 @@ gsap.registerPlugin(ScrollTrigger);
 //     },
 //   });
 // });
-let boxes = gsap.utils.toArray(".projects__box");
+// let boxes = gsap.utils.toArray(".projects__box");
 
-gsap.to(boxes, {
-  x: -300 * (boxes.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".projects__box-wrapper",
-    // pin: true,
-    scrub: true,
-    // snap: 1 / (boxes.length - 1),
-    markers: true,
-    start: "top center",
-    end: "80% center",
-    // end: () => "+=" + document.querySelector(".projects__box").offsetWidth,
-    onEnter: () => console.log("onEnter"),
-  },
-});
+// gsap.to(boxes, {
+//   x: -300 * (boxes.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".projects__box-wrapper",
+//     // pin: true,
+//     scrub: true,
+//     // snap: 1 / (boxes.length - 1),
+//     markers: true,
+//     start: "top center",
+//     end: "80% center",
+//     // end: () => "+=" + document.querySelector(".projects__box").offsetWidth,
+//     onEnter: () => console.log("onEnter"),
+//   },
+// });
 
-gsap.to(".fa-brands", {
-  y: -20,
-  yoyo: true,
-  duration: 0.7,
-  stagger: {
-    amount: 1,
-    repeat: -1,
-    yoyo: true,
-    ease: "power2.in",
-  },
-});
+// gsap.to(".fa-brands", {
+//   y: -20,
+//   yoyo: true,
+//   duration: 0.7,
+//   stagger: {
+//     amount: 1,
+//     repeat: -1,
+//     yoyo: true,
+//     ease: "power2.in",
+//   },
+// });
