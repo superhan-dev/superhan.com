1368dbd82fcf8dfaf4126dace8289e264a82d5fe
superhan-dev <superhan-dev@gmail.com>
2025-01-21 22:14:00 +0900

    r3f 초기 설정

--- apps/web/3d-blog/src/App.tsx
+++ apps/web/3d-blog/src/App.tsx
@@ -1,9 +1,25 @@
+import { ThreeElements, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import * as THREE from 'three'
+import './index.css'
 
-function App() {
-
-  return (
-    <div className="text-red-500">Hello</div>
-  )
+function App(props: ThreeElements['mesh']) {
+const meshRef = useRef<THREE.Mesh>(null!)
+const [hovered, setHover] = useState(false)
+const [active, setActive] = useState(false)
+useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+return (
+  <mesh
+    {...props}
+    ref={meshRef}
+    scale={active ? 1.5 : 1}
+    onClick={(event) => setActive(!active)}
+    onPointerOver={(event) => setHover(true)}
+    onPointerOut={(event) => setHover(false)}>
+    <boxGeometry args={[1, 1, 1]} />
+    <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+  </mesh>
+)
 }
 
-export default App
+export default App;
--- apps/web/3d-blog/src/main.tsx
+++ apps/web/3d-blog/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import { Canvas } from '@react-three/fiber'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Canvas>
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
     <App />
-  </StrictMode>,
-)
+  </Canvas>,
+  )
