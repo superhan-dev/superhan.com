881a3649b89ee2b3b93120116bab68f6c398ea16
superhan-dev <superhan-dev@gmail.com>
2025-01-21 21:59:22 +0900

    3d 블로그 폴더 생성

---
+++ apps/web/3d-blog/src/shared/components/atoms/BgImg.tsx
@@ -0,0 +1,15 @@
+/**
+ * background에 이미지를 넣고 blur처리할 수 있는 기능
+ *  */ 
+
+interface BgImgProps {
+    src: string;
+}
+
+const BgImg = (props:BgImgProps) => {
+  return (
+    <div>BgImg</div>
+  )
+}
+
+export default BgImg
